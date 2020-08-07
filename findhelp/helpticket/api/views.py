0ecf93fb26063422e8f62a93eb6adb1910b2e07c
Yusuf Hilmi <yusufhilmicr@gmail.com>
2020-08-07 19:09:03 +0300

    flake8 linter

--- findhelp/helpticket/api/views.py
+++ findhelp/helpticket/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets  # , permissions
 
 from findhelp.helpticket.api.serializers import HelpTicketSerializer
 from findhelp.helpticket.api.permissions import IsOwnerOrReadOnly
