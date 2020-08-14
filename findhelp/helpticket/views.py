cb62f897ce9991607ce501a4b64210b81bdf2ffa
Yusuf Hilmi <yusufhilmicr@gmail.com>
2020-08-14 12:41:09 +0300

    categorized view added

--- findhelp/helpticket/views.py
+++ findhelp/helpticket/views.py
@@ -1,15 +1,15 @@
-# from django.shortcuts import render
+from django.shortcuts import reverse
 # from django.http import HttpResponse
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 
-from itertools import chain
+# from itertools import chain
 from datetime import timedelta
 from django.utils import timezone
 
-from findhelp.helpticket.models import HelpTicket
+from findhelp.helpticket.models import HelpTicket, HelpCategory
 from findhelp.users.models import User
 
 PAGINATION = 8
@@ -20,13 +20,13 @@ class TicketList(ListView):
     template_name = 'helpticket/tickets.html'
     context_object_name = 'helptickets'
 
-    # def get_context_data(self, **kwargs):
-    #   """"
-    #       Add other things to the context
-    #   """"
-    #     context = super(TicketList, self).get_context_data(**kwargs)
-    #     context['normalhelptickets'] = HelpTicket.objects.filter(category="normal")
-    #     return context
+    def get_context_data(self, **kwargs):
+        """
+            Add other things to the context
+        """
+        context = super(TicketList, self).get_context_data(**kwargs)
+        context['categories'] = HelpCategory.objects.all()
+        return context
 
     def get_queryset(self):
         time_threshold = timezone.now() - timedelta(days=30)
@@ -36,6 +36,21 @@ class TicketList(ListView):
 
 
 ticket_list_view = TicketList.as_view()
+
+
+class TicketListCategorized(ListView):
+    paginate_by = PAGINATION
+    template_name = 'helpticket/tickets.html'
+    context_object_name = 'helptickets'
+
+    def get_queryset(self):
+        time_threshold = timezone.now() - timedelta(days=30)
+        helptickets = HelpTicket.objects.filter(categories=self.kwargs["pk"], updated_at__gt=time_threshold)
+
+        return helptickets
+
+
+ticket_list_categorized_view = TicketListCategorized.as_view()
 
 
 class TicketCreate(LoginRequiredMixin, CreateView):
