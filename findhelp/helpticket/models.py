4f5d11420378d8d28a01e5ea8091b3b43822b18f
Yusuf Hilmi <yusufhilmicr@gmail.com>
2020-08-19 15:39:41 +0300

    Translation fix #2

--- findhelp/helpticket/models.py
+++ findhelp/helpticket/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from findhelp.users.models import User
+from django.utils.translation import ugettext_lazy as _
 
 
 class HelpCategory(models.Model):
@@ -11,11 +12,11 @@ class HelpCategory(models.Model):
 
 
 class HelpTicket(models.Model):
-    categories = models.ManyToManyField(HelpCategory, related_name="ticket_category")
+    categories = models.ManyToManyField(HelpCategory, verbose_name=_('Categories'), related_name="ticket_category")
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ticket_owner")
-    city = models.CharField(max_length=100)
-    description = models.TextField(blank=False)
-    contact = models.CharField(max_length=150,)
+    city = models.CharField(verbose_name=_('City'), max_length=100)
+    description = models.TextField(verbose_name=_('Description'), blank=False)
+    contact = models.CharField(verbose_name=_('Contact'), max_length=150,)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
