21b31e4b1db841e4474cb0872c16b8f8ae0dc241
Yusuf Hilmi <yusufhilmicr@gmail.com>
2020-10-30 23:18:00 +0300

    url naming #2

--- findhelp/helpticket/urls.py
+++ findhelp/helpticket/urls.py
@@ -7,7 +7,7 @@ app_name = "helpticket"
 urlpatterns = [
     path("listings/all", view=views.ticket_list_view, name='home'),
     path("listings/category/<pk>/", view=views.ticket_list_categorized_view, name='categorized'),
-    path("gethelp/", view=views.ticket_create_view, name='create-ticket'),
+    path("create/", view=views.ticket_create_view, name='create-ticket'),
     path("my-listings/update/<pk>/", view=views.ticket_update_view, name='update-ticket'),
     path("my-listings/delete/<pk>/", view=views.ticket_delete_view, name='delete-ticket'),
     path("my-listings/", view=views.profile_ticket_list_view, name='profile')
