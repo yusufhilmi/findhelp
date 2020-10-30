e64cbd164eef5b26bcf15531a31e01cbb2fc0075
Yusuf Hilmi <yusufhilmicr@gmail.com>
2020-10-30 23:05:30 +0300

    url change

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("users/", include("findhelp.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    path("app/", include("findhelp.helpticket.urls", namespace="helpticket")),
+    path("help/", include("findhelp.helpticket.urls", namespace="helpticket")),
     path('i18n/', include('django.conf.urls.i18n')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
